83c34d6b7363ad7d6261e71196f2685cc342a4fa
sainath <sainathbakawade9@gmail.com>
2024-08-13 16:15:34 +0530

    Navigation and contact us implemented

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "../src/App.css"
+import Navigation from "./components/Navigation/Navigation"
+import ContactHeader from "./components/ContactHeader/ContactHeader"
+
+
+const App = () => {
+  return (
+    <>
+    <Navigation/>
+    <ContactHeader/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,21 @@
+import styles from "./Navigation.module.css"
+
+const Navigation = () => {
+  
+  return (
+    <nav className={`${styles.navigation} container`}  >
+        <div className="logo">
+        <img src="" alt="logo" />  
+        </div>
+
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+       
+    </nav>
+  )
+}
+
+export default Navigation
